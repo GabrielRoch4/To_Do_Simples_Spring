ae6ebf809541348944d1225f1f9728ecd858ce52
Gabriel Rocha <111160701+GabrielRoch4@users.noreply.github.com>
2024-02-06 15:36:19 -0300

    Lombok adicionado

--- src/main/java/com/gabriel/todosimple/models/User.java
+++ src/main/java/com/gabriel/todosimple/models/User.java
@@ -1,20 +1,24 @@
 package com.gabriel.todosimple.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 // Como entidade, pode ser tradado como se fosse uma tabela
 @Entity
 // Definindo a Table com o nome definido pela classe
 @Table(name = User.TABLE_NAME)
+@AllArgsConstructor // Contrutor com todos os atributos
+@NoArgsConstructor // Construtor vazio
+@Getter // Getters dos atributos
+@Setter // Setters dos atributos
+@EqualsAndHashCode //
 public class User {
     public interface CreateUser {}
     public interface UpdateUser {}
@@ -47,72 +51,8 @@ public class User {
 
     // One user com várias tasks
     @OneToMany(mappedBy = "user")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // Somente escrita, para não retornar
     private List<Task> tasks = new ArrayList<Task>();
-
-    @JsonIgnore
-    public List<Task> getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(List<Task> tasks) {
-        this.tasks = tasks;
-    }
-
-    public User() {}
-
-    public User(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this)
-            return true;
-        if (obj == null)
-            return false;
-        if (!(obj instanceof User other))
-            return false;
-        if (this.id == null)
-            if (other.id != null)
-                return false;
-            else return false;
-        return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username)
-                && Objects.equals(this.password, other.password);
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
-        return result;
-    }
 
 }
 
